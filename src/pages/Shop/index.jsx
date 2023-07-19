f9445474ddf238338f67e1ccaf5644433c83d4d0
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2023-07-19 23:59:43 +0200

    get started created first draft

--- src/pages/Shop/index.jsx
+++ src/pages/Shop/index.jsx
@@ -2,9 +2,12 @@ import React, { useContext } from "react";
 import Layout from "components/Layout";
 import { LangContext } from "utils/LangContext";
 import { content } from "constants/content";
+import WhiteCard from "images/white-card-water.png";
+import BambooCard from "images/bamboo-card.png";
+import { Button } from "techhype-components";
 
 const Shop = () => {
-  const [lang, ] = useContext(LangContext);
+  const [lang] = useContext(LangContext);
 
   return (
     <Layout
@@ -14,7 +17,32 @@ const Shop = () => {
       <section className="shop top-overlay">
         <div className="container-inner shop-container">
           <h1>{content[lang]["shopHeading"]}</h1>
-          <p>Page under Construction...</p>
+          <div className="product-grid">
+            <div className="product-item">
+              <div className="product-image">
+                <img src={WhiteCard} alt="White Business Card" />
+              </div>
+              <div className="product-info">
+                <div className="product-text">
+                  <h2>Plastic Card White</h2>
+                  <span>299,00 NOK</span>
+                </div>
+                <Button size="small" style={{ flex: 1 }}>Select</Button>
+              </div>
+            </div>
+            <div className="product-item">
+              <div className="product-image">
+                <img src={BambooCard} alt="Bamboo Business Card" />
+              </div>
+              <div className="product-info">
+                <div className="product-text">
+                  <h2>Bamboo Wooden Card</h2>
+                  <span>399,00 NOK</span>
+                </div>
+                <Button size="small" style={{ flex: 1 }}>Select</Button>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </Layout>
