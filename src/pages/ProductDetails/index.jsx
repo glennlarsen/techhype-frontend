8d71f7fab10578ff8cd8e503a27f1d5f31fae3a3
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2023-07-21 16:32:09 +0200

    added add to cart modal first draft

--- src/pages/ProductDetails/index.jsx
+++ src/pages/ProductDetails/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useForm, Controller } from "react-hook-form";
 import Layout from "components/Layout";
@@ -9,6 +9,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import schema from "constants/schema";
 import { LangContext } from "utils/LangContext";
 import { content } from "constants/content";
+import { useNavigate } from "react-router-dom";
 
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
@@ -22,10 +23,35 @@ import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Backdrop from '@mui/material/Backdrop';
+import Fade from '@mui/material/Fade';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  minWidth: "300px",
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  color: 'black',
+  borderRadius: "10px",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  p: 4,
+};
 
 const ProductDetails = () => {
   const [lang] = useContext(LangContext);
   const { id } = useParams(); // Get the cardId from the URL
+  const [quantity, setQuantity] = useState(1);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const navigate = useNavigate();
 
   // Find the product with the matching id
   const product = products.find((product) => product.id === parseInt(id, 10));
@@ -74,6 +100,15 @@ const ProductDetails = () => {
   } = useForm({
     resolver: yupResolver(schema),
   });
+
+  const handleChange = (event) => {
+    setQuantity(event.target.value);
+  };
+
+  const handleAddToCart = () => {
+    console.log(quantity);
+    handleOpen(quantity);
+  };
 
   if (!product) {
     // Handle if product not found with the given id
@@ -134,6 +169,8 @@ const ProductDetails = () => {
                             },
                           }}
                           {...field}
+                          onChange={handleChange}
+                          value={quantity}
                         >
                           <MenuItem value={1}>1</MenuItem>
                           <MenuItem value={2}>2</MenuItem>
@@ -148,7 +185,9 @@ const ProductDetails = () => {
                     />
                   </FormControl>
                 </ThemeProvider>
-                <Button size="small">{content[lang]["addToCart"]}</Button>
+                <Button size="small" onClick={handleAddToCart}>
+                  {content[lang]["addToCart"]}
+                </Button>
               </div>
 
               <div className="details-text">
@@ -158,12 +197,12 @@ const ProductDetails = () => {
                     aria-controls="panel1a-content"
                     id="panel1a-header"
                   >
-                    <Typography variant="subtitle1" sx={{fontWeight: "bold"}}>
+                    <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
                       {content[lang]["detailsAccordionHeader"]}
                     </Typography>
                   </AccordionSummary>
                   <AccordionDetails>
-                    <p style={{marginTop: 0}}>{content[lang]["details1"]}</p>
+                    <p style={{ marginTop: 0 }}>{content[lang]["details1"]}</p>
                     <p>{content[lang]["details2"]}</p>
                     <p>{content[lang]["details3"]}</p>
                   </AccordionDetails>
@@ -174,14 +213,12 @@ const ProductDetails = () => {
                     aria-controls="panel2a-content"
                     id="panel2a-header"
                   >
-                    <Typography variant="subtitle1" sx={{fontWeight: "bold"}}>
+                    <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
                       {content[lang]["warrantyAccordionHeader"]}
                     </Typography>
                   </AccordionSummary>
                   <AccordionDetails>
-                    <Typography>
-                    {content[lang]["warrantyText"]}
-                    </Typography>
+                    <Typography>{content[lang]["warrantyText"]}</Typography>
                   </AccordionDetails>
                 </Accordion>
                 <Accordion>
@@ -190,29 +227,23 @@ const ProductDetails = () => {
                     aria-controls="panel2a-content"
                     id="panel2a-header"
                   >
-                    <Typography variant="subtitle1" sx={{fontWeight: "bold"}}>
+                    <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
                       {content[lang]["FaqAccordionHeader"]}
                     </Typography>
                   </AccordionSummary>
                   <AccordionDetails>
                     <Typography variant="subtitle2">
-                    {content[lang]["faqQuestion1"]}
+                      {content[lang]["faqQuestion1"]}
                     </Typography>
-                    <Typography>
-                    {content[lang]["faqAnswer1"]}
-                    </Typography>
+                    <Typography>{content[lang]["faqAnswer1"]}</Typography>
                     <Typography variant="subtitle2" mt={2}>
-                    {content[lang]["faqQuestion2"]}
+                      {content[lang]["faqQuestion2"]}
                     </Typography>
-                    <Typography>
-                    {content[lang]["faqAnswer2"]}
-                    </Typography>
+                    <Typography>{content[lang]["faqAnswer2"]}</Typography>
                     <Typography variant="subtitle2" mt={2}>
-                    {content[lang]["faqQuestion3"]}
+                      {content[lang]["faqQuestion3"]}
                     </Typography>
-                    <Typography>
-                    {content[lang]["faqAnswer3"]}
-                    </Typography>
+                    <Typography>{content[lang]["faqAnswer3"]}</Typography>
                   </AccordionDetails>
                 </Accordion>
               </div>
@@ -220,6 +251,38 @@ const ProductDetails = () => {
           </div>
         </div>
       </section>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={open}>
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Added to Cart!
+          </Typography>
+          <Typography>
+            {quantity} x {product.name} ({product.price} NOK)
+          </Typography>
+          <img src={product.image[0]} alt="" />
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+           <strong>Total</strong> {parseInt(product.price) * quantity},00 NOK
+          </Typography>
+          <Button style={{background: "transparent", border: "2px solid #54d4c6", margin: "1em"}} size="small" onClick={handleClose}>Close</Button>
+          <Button size="small" onClick={(quantity) => navigate("/cart")}>Continue to Cart</Button>
+        </Box>
+        </Fade>
+      </Modal>
+
     </Layout>
   );
 };
