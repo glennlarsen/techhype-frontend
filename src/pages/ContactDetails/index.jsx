416931c578a658b7a882bc815d5ff954abbac1ac
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2024-04-21 16:02:23 +0200

    added new contact details and conditions for missing values

--- src/pages/ContactDetails/index.jsx
+++ src/pages/ContactDetails/index.jsx
@@ -1,10 +1,13 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Button } from "techhype-components";
 import VCard from "vcard-creator";
 import Glenn from "images/glenn.jpg";
 import Cato from "images/cato-printingas.jpg";
+import Akam from "images/akam.jpg";
+import Gunnar from "images/Gunnar.jpg";
 import PlaceholderAvatar from "images/placeholder-avatar.jpg";
+import Logo from "logo/logo-long.png";
 
 import { Stack } from "@mui/system";
 import { color_dark, color_darker } from "constants/colors";
@@ -17,41 +20,45 @@ import ContactHeading from "./components/ContactHeading";
 import DividerComponent from "./components/DividerComponent";
 import TextFieldComponent from "./components/TextFieldComponent";
 
-import contactInfoData  from "./data/contactInfo.json";
+import contactInfoData from "./data/contactInfo.json";
 
 const ContactDetails = () => {
   const isBigScreen = useMediaQuery({ minWidth: 710 });
   const { id } = useParams();
   const [contactInfo, setContactInfo] = useState(null);
-  const [contactImage, setContactImage] = useState(null);  
+  const [contactImage, setContactImage] = useState(null);
 
   console.log("id param:", id);
   console.log("Contact info:", contactInfo);
 
-    // Effect to load the contact information based on the ID
-    useEffect(() => {
-      const foundContact = contactInfoData.find(contact => contact.id.toString() === id);
-      if (foundContact) {
-        setContactInfo(foundContact);
-      } else {
-        // Handle the case where no contact is found
-        console.error("No contact found for ID:", id);
-      }
-    }, [id]);
+  // Effect to load the contact information based on the ID
+  useEffect(() => {
+    const foundContact = contactInfoData.find(
+      (contact) => contact.id.toString() === id
+    );
+    if (foundContact) {
+      setContactInfo(foundContact);
+    } else {
+      // Handle the case where no contact is found
+      console.error("No contact found for ID:", id);
+    }
+  }, [id]);
 
+  useEffect(() => {
+    if (id === "892346788234") {
+      setContactImage(Glenn);
+    } else if (id === "592356784215") {
+      setContactImage(Cato);
+    } else if (id === "592656784315") {
+      setContactImage(Akam);
+    } else if (id === "992656754335") {
+      setContactImage(Gunnar);
+    } else {
+      setContactImage(PlaceholderAvatar);
+    }
+  }, [id]); // Depend on id to only run when id changes
 
-    useEffect(() => {
-      if (id === "892346788234") {
-        setContactImage(Glenn);
-      } else if (id === "592356784215") {
-        setContactImage(Cato);
-      } else {
-        setContactImage(PlaceholderAvatar);
-      }
-    }, [id]); // Depend on id to only run when id changes
-    
-
-     // If contactInfo is not yet set, you can render a loading indicator or return null
+  // If contactInfo is not yet set, you can render a loading indicator or return null
   if (!contactInfo) {
     return <div>No contact info found</div>; // or some loading spinner
   }
@@ -75,7 +82,8 @@ const ContactDetails = () => {
         const vCard = new VCard();
 
         // Set properties
-        vCard.addName(contactInfo.name);
+        vCard.addName(contactInfo.lastName, contactInfo.firstName);
+        vCard.addCompany(contactInfo.company);
         vCard.addPhoneNumber(contactInfo.phone, "CELL"); // Set phone number as mobile
         vCard.addEmail(contactInfo.email, "INTERNET"); // Set email as email
         vCard.addJobtitle(contactInfo.title);
@@ -106,7 +114,7 @@ const ContactDetails = () => {
         // Trigger the download
         const link = document.createElement("a");
         link.href = URL.createObjectURL(vCardBlob);
-        link.download = `${contactInfo.name}.vcf`;
+        link.download = `${contactInfo.firstName}.vcf`;
         link.click();
         URL.revokeObjectURL(link.href);
       };
@@ -115,10 +123,24 @@ const ContactDetails = () => {
     }
   };
 
- 
+  const addressComponents = [
+    contactInfo.address,
+    contactInfo.postcode,
+    contactInfo.city,
+    contactInfo.country,
+  ].filter(Boolean); // Filter out empty/undefined values
+
+  const addressString = addressComponents.join(", ");
 
   return (
-    <Box sx={{ background: color_dark, height: "100vh", display: "flex", flexFlow: "column" }}>
+    <Box
+      sx={{
+        background: color_dark,
+        height: "100vh",
+        display: "flex",
+        flexFlow: "column",
+      }}
+    >
       <Stack
         style={{
           padding: "1em",
@@ -132,22 +154,61 @@ const ContactDetails = () => {
         }}
       >
         <Avatar src={contactImage} />
-        <ContactHeading title={contactInfo.title} value={contactInfo.name} />
-        <List sx={{width: "100%"}}>
-        <DividerComponent />
-        <TextFieldComponent label="Phone" value={contactInfo.phone} />
-        <DividerComponent />
-        <TextFieldComponent label="Email" value={contactInfo.email} />
-        <DividerComponent />
-        <TextFieldComponent label="Address" value={`${contactInfo.address}, ${contactInfo.postcode} ${contactInfo.city}, ${contactInfo.country}`} />
-        <DividerComponent />
-        <TextFieldComponent label="Website" value={contactInfo.website}/>
-        <DividerComponent />
-        {/* Add other contact details as needed */}
-        <Button style={{ marginTop: "2em" }} onClick={handleDownloadContact}>
-          Save Contact
-        </Button>
+        <ContactHeading
+          title={contactInfo.title || ""}
+          value={
+            (contactInfo.firstName ? contactInfo.firstName : "") +
+            (contactInfo.lastName ? " " + contactInfo.lastName : "")
+          }
+        />
+        <List sx={{ width: "100%" }}>
+          <DividerComponent />
+          {contactInfo.phone ? (
+            <>
+              <TextFieldComponent label="Phone" value={contactInfo.phone} />
+              <DividerComponent />
+            </>
+          ) : null}
+          {contactInfo.email ? (
+            <>
+              <TextFieldComponent label="Email" value={contactInfo.email} />
+              <DividerComponent />
+            </>
+          ) : null}
+          {addressString ? (
+            <>
+              <TextFieldComponent label="Address" value={addressString} />
+              <DividerComponent />
+            </>
+          ) : null}
+          {contactInfo.website ? (
+            <>
+              <TextFieldComponent label="Website" value={contactInfo.website} />
+              <DividerComponent />
+            </>
+          ) : null}
+          {contactInfo.company ? (
+            <>
+              <TextFieldComponent label="Company" value={contactInfo.company} />
+              <DividerComponent />
+            </>
+          ) : null}
+          {/* Add other contact details as needed */}
+          <Button style={{ marginTop: "2em" }} onClick={handleDownloadContact}>
+            Save Contact
+          </Button>
         </List>
+        <Box class="poweredby-container">
+          Powered by{" "}
+          <a
+            class="poweredBy"
+            href="http://techhype.no"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Logo} alt="Techhype Logo" />
+          </a>
+        </Box>
       </Stack>
     </Box>
   );
